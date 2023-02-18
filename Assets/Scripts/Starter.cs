c8dd9148492df766e19a38e0a70ad8d23232e9e3
Sergey Rodionov <63009193+Rodser@users.noreply.github.com>
2023-02-18 17:46:04 +0400

    WorldGenerator

--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -4,26 +4,18 @@ namespace Rodlix
 {
     public class Starter : MonoBehaviour
     {
-        [SerializeField] private byte size = 10;
-        [SerializeField] private BlockGenerator[] generators = null;
+        [SerializeField] private WorldGenerator generator = null;
 
         private Block[,,] blocks = null;
 
         private void Start()
         {
-            blocks = new Block[size, size, size];
+            blocks = generator.StartGeneration();
 
-            StartGeneration();
+
             //  renderer
             //  player
         }
 
-        private void StartGeneration()
-        {
-            foreach (var generator in generators)
-            {
-                generator.GenerateBlocks(blocks, size);
-            }
-        }
     }
 }
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using UnityEngine;
+
+namespace Rodlix
+{
+    [CreateAssetMenu(fileName = "WorldGenerator", menuName = "MyGame/WorldGenerator", order = 0)]
+    public class WorldGenerator : ScriptableObject
+    {
+        [SerializeField] private byte worldSize = 10;
+        [SerializeField] private BlockGenerator[] generators = null;
+
+        public Block[,,] StartGeneration()
+        {
+            Block[,,] blocks = new Block[worldSize, worldSize, worldSize];
+            
+            foreach (var generator in generators)
+            {
+                generator.GenerateBlocks(blocks, worldSize);
+            }
+
+            return blocks;
+        }
+
+        // граница
+        // грунт
+        // жидкость
+        // газ
+        // предметы
+        // живность
+        // игрок
+    }
+}
