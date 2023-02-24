3daf775cdcccac99c63c2d5afe11db392c7759b9
Sergey Rodionov <63009193+Rodser@users.noreply.github.com>
2023-02-24 12:55:47 +0400

    BlockRenderer
    without WorldGenerator

---
+++ Assets/Scripts/BlockRenderer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Rodlix
+{
+    public class BlockRenderer : MonoBehaviour
+    {
+        private Vector3Int worldSize;
+
+        public void SetWorldSize(Vector3Int worldSize)
+        {
+            this.worldSize = worldSize;
+        }
+
+        public GameObject[,,] Generate(Block[,,] blocks)
+        {
+            GameObject[,,] gameObjects = new GameObject[worldSize.x, worldSize.y, worldSize.z];
+
+            for (int y = 0; y < worldSize.y; y++)
+            {
+                for (int x = 0; x < worldSize.x; x++)
+                {
+                    for (int z = 0; z < worldSize.z; z++)
+                    {
+                        Block block = blocks[x, y, z];
+                        GameObject prefab = block?.prefab;
+                        if (prefab != null)
+                        {
+                            Vector3 position = new Vector3(x, y, z);
+                            GameObject gameObject = Instantiate(prefab, position, Quaternion.identity);
+                            gameObject.GetComponent<Renderer>().material = block.material;
+                            gameObjects[x, y, z] = gameObject;
+                        }
+                    }
+                }
+            }
+            return gameObjects;
+        }
+    }
+}
--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -7,16 +7,20 @@ namespace Rodlix
         [SerializeField] private WorldGenerator generator = null;
         [SerializeField] private Base baseBlocks = null;
 
+        private BlockRenderer blockRenderer = null;
         private GameObject[,,] gameObjects = null;
 
         private void Start()
         {
-            gameObjects = generator.StartGeneration(baseBlocks);
-
+            Block[,,] blocks = generator.StartGeneration(baseBlocks);
 
             //  renderer
+            Vector3Int worldSize = generator.WorldSize;
+            blockRenderer = gameObject.AddComponent<BlockRenderer>();
+            blockRenderer.SetWorldSize(worldSize);
+            gameObjects = blockRenderer.Generate(blocks);
+
             //  player
         }
-
     }
 }
